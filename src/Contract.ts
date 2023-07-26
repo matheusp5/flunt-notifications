46004844681b388250fb4bc269e48582f09890b2
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-26 12:53:38 -0300

    Fix: bad implementation at hasSpecificLength. Error at compare (finished compare section).

--- src/Contract.ts
+++ src/Contract.ts
@@ -1,4 +1,6 @@
 ﻿import Notification from './Models/Notification';
+import {CompareTypes} from "@src/Enums/CompareTypesEnum";
+import CompareService from "@src/Utils/CompareService";
 
 export default class Contract<T extends Notification> {
   public _notifications: T[] = [];
@@ -18,13 +20,13 @@ export default class Contract<T extends Notification> {
     return this._notifications;
   }
 
-  public hasSpecifiedLength(value: string, length: number, notification: T) {
-    if (value.length === length) this._notifications.push(notification);
+  public hasSpecificLength(value: string, length: number, notification: T) {
+    if (value.length !== length) this._notifications.push(notification);
     return this._notifications;
   }
 
-  public compare(value: number, value2: number, notification: T) {
-    if (value.length === length) this._notifications.push(notification);
+  public compare<R>(value: R, value2: R, type: CompareTypes, notification: T) {
+    if(!CompareService.Compare(value, value2, type)) this._notifications.push(notification);
     return this._notifications;
   }
 }
