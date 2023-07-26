5ea4710eb3552c5d290c78a319d276710eccc522
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-26 14:10:48 -0300

    RegEx Patterns

---
+++ src/Utils/Patterns.ts
@@ -0,0 +1,7 @@
+﻿class Patterns {
+  public Email: RegExp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+  public CPF: RegExp = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/
+  public CEP: RegExp = /^\d{5}-\d{3}$/
+  public Date: RegExp = /^(0[1-9]|[1-2]\d|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/
+  public DateTime: RegExp = /^(0[1-9]|[1-2]\d|3[0-1])\/(0[1-9]|1[0-2])\/\d{4} (0\d|1\d|2[0-3]):[0-5]\d$/
+}
