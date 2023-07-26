01fc0dbb736412001c21e342fed145cd25362a15
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-26 14:22:34 -0300

    Jest config for tests.

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+﻿module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'], // Ignore the 'dist' folder
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
