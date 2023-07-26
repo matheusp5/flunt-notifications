234ab1f7424b4eb069f3dd12508f3901c0098856
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-07-26 12:28:36 -0300

    Format configuration, Notification model.

---
+++ src/Models/Notification.ts
@@ -0,0 +1,9 @@
+﻿
+export default class Notification {
+  public _property: string;
+  public _message: string;
+  constructor(property: string, message: string) {
+    this._property = property
+    this._message = message
+  }
+}
